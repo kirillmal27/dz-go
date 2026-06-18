08e49b0a5478f5180589da2e8d8f1d9473bdcf33
Kirill Logunov <6596959@gmail.com>
2026-06-18 16:02:08 +0300

    second commit

--- main.go
+++ main.go
@@ -6,9 +6,25 @@ import (
 )
 
 func main() {
-	userHeight := 1.82
-	userWeight := 95.0
-	BMI := userWeight / math.Pow(userHeight, 2);
+	fmt.Println("__ Калькулятор индекса массы тела __")
+	userWeight, userHeight := getUserInput()
+
+	BMI := calculateBMI(userWeight, userHeight)
 
 	fmt.Printf("Your BMI is: %.2f", BMI)
+}
+
+func getUserInput() (float64, float64) {
+	var userHeight float64
+	var userWeight float64
+	fmt.Println("Введите свой рост в метрах = ")
+	fmt.Scan(&userHeight)
+	fmt.Println("Введите свой вес в кг = ")
+	fmt.Scan(&userWeight)
+
+	return userWeight, userHeight
+}
+
+func calculateBMI(userWeight float64, userHeight float64) float64 {
+	return userWeight / math.Pow(userHeight, 2);
 }

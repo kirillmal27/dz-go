6d648ba26aff17ee7d3deb5140fac7cad6e0b0da
Kirill Logunov <6596959@gmail.com>
2026-06-18 16:06:13 +0300

    second home work

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+func getUserInput() float64 {
+	var input float64
+
+	fmt.Scan(&input)
+
+	return input
+}
+
+func convertMoney(money float64, currentCurrency string, intoCurrency string) {}
+
 func main() {
 	const usdToEur = 0.92
 	const usdToRub = 91.50
